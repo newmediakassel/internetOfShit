90d3f02caa4ca7cb5c148fa3f411f6ed5f25c147
Jörn Röder <kontakt@joernroeder.de>
2016-05-17 17:41:22 +0200

    updated wind turbine

--- wind-turbine.js
+++ wind-turbine.js
@@ -1,10 +1,31 @@
 'use strict'; // use javascript strict mode
 
-const five = require('johnny-five');
-const board = new five.Board();
+//const five = require('johnny-five');
+//const board = new five.Board();
+const xzeresWindTurbine = require('./xzeres-wind-turbine');
+
+// --- CONFIG ---
+const TURBINE_URLS = [
+  'http://81.153.53.164',
+  'http://74.43.48.185',
+  'http://206.212.238.180'
+];
 
 // function to call after a connection to the board is established
-const onBoardReady = function () {
+/*const onBoardReady = function () {
+
 };
 
-board.on('ready', onBoardReady);
+// kick things off!
+board.on('ready', onBoardReady);*/
+TURBINE_URLS.forEach(function (url) {
+  xzeresWindTurbine.getData(url, function (err, data) {
+    if (err) { // error handling
+      console.log(err);
+      return;
+    }
+
+    console.log('got data from', url, data);
+    // data!!!!
+  })
+});
